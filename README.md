265793bbf9df387c509275ece6e04d8a10bbd601
Patrick Felt <fatpelt@gmail.com>
2022-09-07 14:00:32 -0600

    - initial commit
    Signed-off-by: Patrick Felt <fatpelt@gmail.com>

---
+++ README.md
@@ -0,0 +1,52 @@
+# Arista Auto Port Config Agent
+Spiritual successor to the work by https://github.com/jonathansm/arista-scripts.
+
+This EOS agent will configure interfaces based on MAC or OUI connected to ports when they become operational.  Configuration data can be stored in either YAML or JSON format and is stored in flash.  Ports can also be configured to a default state when they switch to the link down state.
+
+## Config
+Configuration data is stored in either /mnt/flash/autoPortConfigAgent.json or /mnt/flash/autoPortConfigAgent.yml depending on which configuration language is preferred.  the YAML configuration file will be attempted first falling back to the json file.  Examples of both formats are provided in this repository.
+
+
+## Run Location and parameters
+This python agent should be stored in /mnt/flash and is run using the EOS daemon syntax.  There is currently one configuration option
+
+- "interfaces" an EOS configuration string representing the interfaces that you'd like to monitor.  This string should follow the same syntax as specifying a range in cli configuration mode.  Interface names will be resolved internally to their proper fully qualified forms.  For example: specifying "e1-4" will be automatically expanded as needed to include Ethernet1 through Ethernet4 inclusive.  The use of the keyword "all", or not setting an interfaces option at all, can be used to monitor all interfaces, however this should be used with caution as it may reconfigure uplink ports and disconnect the switch from the network!
+
+
+### Daemon configuration
+Local access to api management interfaces must be configured for this agent to function properly.  This can be done with configuration similar to
+```
+management api http-commands
+   protocol unix-socket
+   no shutdown
+   !
+   vrf Management
+      no shutdown
+!
+```
+
+Configuration of the daemon is accomplished as follows
+
+```
+daemon portSet
+   exec /mnt/flash/autoPortConfigAgent.py
+   option interfaces value all
+   no shutdown
+!
+```
+
+### Detailed operation
+This agent will parse the configuration files for lists of either specific mac addresses or oui addresses and monitor a set of interfaces for linkup and lindown events.
+
+#### linkup event
+Upon a linkup event for a monitored interface, the agent will begin watching for new mac addresses to be learned on that interface. When a mac address is learned the agent will,
+1. Disable mac address monitoring for that interface
+2. Search the configuration for a mac address or oui address match
+3. Apply matched specific mac configuration, or if there is no specific match, apply the matched oui confiugration to the interface.
+4. Continue monitoring for link state change notifications
+
+#### linkdown event
+If an interface transitions to a linkdown state, a default configuration can be set on the interface.  If no default is specified in the configuration, no changes will be made.
+
+### CVP warning
+This script does not interface with CVP.  As such any configuration applied to the switch may cause the switch to show as out-of-sync within any CVP instance to which this switch is tied.  Manual reconciliation would be required.
---
+++ autoPortConfigAgent.py
@@ -0,0 +1,205 @@
+#!/usr/bin/env python3
+# https://github.com/aristanetworks/EosSdk/blob/master/examples/IntfIpAddrMergeExample.py
+#### http://aristanetworks.github.io/EosSdk/docs/2.19.0/ref/
+
+import eossdk, yaml, json, sys, pyeapi, uuid
+
+def formatMac(mac):
+    return mac.replace(':', '').replace('.','').replace('-', '').strip().lower()
+
+class InterfaceMonitor(eossdk.AgentHandler, eossdk.IntfHandler, eossdk.MacTableHandler):
+    def log(self, sev, outStr):
+        if sev == 0:
+            self.tracer.trace0(outStr)
+            print(outStr)
+        elif sev == 5:
+            self.tracer.trace5(outStr)
+
+    def __init__(self, intfMgr, agentMgr, macMgr):
+        eossdk.AgentHandler.__init__(self, agentMgr)
+        eossdk.IntfHandler.__init__(self, intfMgr)
+        eossdk.MacTableHandler.__init__(self, macMgr)
+        self.tracer = eossdk.Tracer("EosSdkInterfaceAutoConfigure")
+        self.intfMgr_ = intfMgr
+        self.agentMgr_ = agentMgr
+        self.macTableMgr_ = macMgr
+        self.pyeapi = pyeapi.connect_to("localhost")
+        self.interestingInterfaces = []
+
+        self.configFile = "/mnt/flash/autoPortConfig.yml"
+
+    def on_agent_option(self, optionName, value):
+        # then if we have a new batch of interfaces to watch, let's figure them out
+        if optionName == "interfaces":
+            # turn off any monitoring that's already on
+            self.log(5, "Disabling all interface monitoring")
+            self.watch_all_intfs(False)
+            self.watch_all_mac_entries(False)
+
+            # we are wanting to monitor a new set of interfaces
+            #  first, use pyeapi to pull the interface names
+            #  of the requisite interfaces. "all" or "" will
+            #  not limit the interfaces we are looking at
+            t = []
+            cmd = 'show int {} stat'.format(value)
+            try:
+                t = self.pyeapi.enable(cmd, autoComplete=True)
+            except:
+                pass
+
+            if len(t) > 0:
+                self.interfaces = t[0].get('result', []).get('interfaceStatuses',[])
+                # loop over any interfaces that we get back from pyeapi
+                #   and start the operstatus monitoring for each
+                for intf in self.interfaces:
+                    # grab a handle for this interface from eossdk
+                    self.log(1, "monitoring interface {}".format(intf))
+                    self.watch_intf(eossdk.IntfId(intf), True)
+
+    def on_initialized(self):
+        """ Callback provided by AgentHandler when all state is synchronized """
+        # load up the configuration
+        self.configs = {}
+
+        try:
+            with open(self.configFile, "r") as configFile:
+                # let's try yaml first, then fall to json
+                try:
+                    self.log(5, "trying yaml")
+                    self.configs = yaml.safe_load(configFile)
+                    self.log(1, " - successfully loaded the config as yml")
+                except:
+                    # we failed loading yaml.  let's try json
+                    try:
+                        self.log(5, "trying json")
+                        self.configs = json.load(configFile)
+                        self.log(1, " - successfully loaded the config as json")
+                    except:
+                        pass
+        except:
+            self.log(0, "Could not find the configuration file")
+
+        if len(self.configs) == 0:
+            self.log(0, "Error loading the configuration")
+            return
+
+        # now we need to reformat all the macs and ouis to something consistent and usable
+        for config in self.configs['configs']:
+            for ar in ['macs', 'ouis']:
+                config['config'][ar] = list(map(formatMac, config['config'].get(ar, [])))
+
+        # by default eossdk doesn't parse the options on load.  we need
+        #  to fake the call this will return the option interfaces which
+        #  we'll use to determine what to watch.  "all" or "" needs to
+        #  reset the value to "" so that pulling the interfaces from pyeapi
+        #  will just pull them all
+        intfs = self.agentMgr_.agent_option("interfaces")
+        if intfs in ("", "all"):
+            intfs = ""
+            self.log(0, "No specific interfaces have been set to be monitored!, monitoring everything")
+
+        self.on_agent_option("interfaces", intfs)
+        self.log(0, "Fully initialized, running")
+
+    def on_oper_status(self, intfId, operState):
+        """ Callback provided by IntfHandler when an interface's
+        configuration changes """
+
+        # when we get an interface state change we need to turn on mac address table monitoring.
+        #  unfortunately with the sdk there is no way to filter the alerts based on interface
+        #  so we will get a lot of mac address notices, potentially including for interfaces
+        #  we have already processed and don't want to process again.
+        intfStr = intfId.to_string()
+
+        if operState == eossdk.INTF_OPER_UP:
+            # searching the list should probably be a really quick loop as there aren't likely
+            #   to be a lot of interfaces in the coming up state at the same time
+            if not intfStr in self.interestingInterfaces:
+                self.interestingInterfaces.append(intfStr)
+
+            # this call should be a noop if it's already on, but start monitoring for mac
+            #   learns in the mac table
+            self.watch_all_mac_entries(True)
+        elif operState == eossdk.INTF_OPER_DOWN:
+            # set the default and remove this interface from the list.  if it's the last one,
+            #  turn off table monitoring.  remove will except... is this better than a try
+            #  block?
+            if intfStr in self.interestingInterfaces:
+                self.interestingInterfaces.remove(intfStr)
+
+            if len(self.interestingInterfaces) == 0:
+                self.watch_all_mac_entries(False)
+
+            # set the interface to a default if one exists
+            portConfig = self.configs.get('default', [])
+            if 'states' in portConfig and 'linkdown' in portConfig['states']:
+                defaultCommands = portConfig['states']['linkdown']
+                sessionID = uuid.uuid1()
+                commandSequence = ['configure session {}'.format(sessionID),
+                        'default interface {}'.format(intfStr),
+                        'interface {}'.format(intfStr) ] +defaultCommands + ['commit']
+                self.log(0, "Defaulting interface {}".format(intfStr))
+                self.pyeapi.config(commandSequence)
+
+    def on_mac_entry_set(self, mac):
+        # .intfs() will return a set of all the interfaces that this mac has been found on
+        #   we need to loop over all of them and set each interface accordingly
+        intfIds = mac.intfs()
+        for intf in intfIds:
+            # loop over all the interfaces for this mac address.  if it is in our monitored
+            #   list we can remove it and run the requisite change to the interface if there
+            #   is a match
+            intfStr = intf.to_string()
+            if intfStr in self.interestingInterfaces:
+                # we're processing this interface, regardless as to if there is a match.  we
+                #   should remove it from the monitored list
+                self.interestingInterfaces.remove(intfStr)
+
+                if len(self.interestingInterfaces) == 0:
+                    self.watch_all_mac_entries(False)
+
+                macStr = mac.mac_key().eth_addr().to_string()
+                portConfig = self.search(formatMac(macStr))
+                if not portConfig:
+                    self.log(2, "we didn't find a match for mac {}".format(macStr))
+                    return
+
+                if 'states' in portConfig and 'linkup' in portConfig['states']:
+                    self.log(0, "Setting a configuration on {}".format(intfStr))
+                    self.configureInterface(intfStr, portConfig['states']['linkup'])
+
+    def configureInterface(self, intfStr, portConfig):
+        sessionID = uuid.uuid1()
+        commandSequence = ['configure session {}'.format(sessionID),
+                'default interface {}'.format(intfStr),
+                'interface {}'.format(intfStr) ] + portConfig + ['commit']
+        self.pyeapi.config(commandSequence)
+
+    def search(self, mac):
+        ouiResult = None
+        macResult = None
+
+        # main search loop
+        self.log(1, "searching for {}".format(mac))
+        for config in self.configs['configs']:
+            # look for specific matches for each mac address in the mac table
+            if mac in config['config']['macs']:
+                self.log(1, "found a specific match for {} in {}".format(mac, config))
+                macResult = config['config']
+            # look for oui matches
+            if mac[:6] in config['config']['ouis']:
+                self.log(1, "found an oui match for {} in {}".format(mac, config))
+                ouiResult = config['config']
+
+        if macResult:
+            return macResult
+        elif ouiResult:
+            return ouiResult
+        else:
+            # we didn't find any mac or oui match.  if there is a default, let's use it
+            return self.configs.get('default', None)
+
+if __name__ == "__main__":
+    sdk = eossdk.Sdk()
+    _ = InterfaceMonitor(sdk.get_intf_mgr(), sdk.get_agent_mgr(), sdk.get_mac_table_mgr())
+    sdk.main_loop(sys.argv)
